102a0c9def87c2da04216e9f9ad4da0a680db3b1
Mikkel Nygaard <mikkel@bnyg.dk>
2021-06-15 20:42:29 +0200

    Added a better error page.

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,9 +1,20 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">{{ pageNotFound }}</h1>
-    <h1 v-else>{{ otherError }}</h1>
-    <NuxtLink to="/">Home page</NuxtLink>
-  </v-app>
+  <v-container>
+    <v-row>
+      <v-col align="center">
+        <v-avatar size="400" roun
+          ><v-img :src="require('~/assets/images/error.jpg')"></v-img
+        ></v-avatar>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col align="center">
+        <h1 v-if="error.statusCode === 404">{{ pageNotFound }}</h1>
+        <h1 v-else>{{ otherError }}</h1>
+        <NuxtLink to="/">Home page</NuxtLink>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
